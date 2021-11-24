ab1f4a5dadd71a6a9e688a9e02be7bf24e9df62e
Opilane TTHK <opilane@tthk.ee>
2021-11-24 09:28:21 +0200

    projekti loomine

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TARpe19TodoApp.Models;
+
+namespace TARpe19TodoApp.Data
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public DbSet<ItemData> Items { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/Dto/Response/RegistrationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TARpe19TodoApp.Domain;
+
+namespace TARpe19TodoApp.Models.Dto.Response
+{
+    public class RegistrationResponse : AuthResult
+    {
+
+    }
+}
